43f6ffcf0027838c2b009d101ab97986082c2eb6
AZ <47402666+realazaber@users.noreply.github.com>
2025-02-12 07:04:43 +1100

    Backend - Cleanup: Removed temporary testing code.

--- backend/Extensions/RegisterStartupMiddleware.cs
+++ backend/Extensions/RegisterStartupMiddleware.cs
@@ -10,29 +10,7 @@ namespace backend.Extensions
         public static WebApplication RegisterMiddleware(this WebApplication app)
         {
             app.UseHttpsRedirection();
-            app.UseCors("AllowLocalhost4200");
-            //app.UseAuthentication();
-            //app.UseAuthorization();
-            app.Use(async (context, next) =>
-            {
-                var user = context.User;
-                if (user.Identity.IsAuthenticated)
-                {
-                    Console.WriteLine($"✅ User authenticated: {user.Identity.Name}");
-                    foreach (var claim in user.Claims)
-                    {
-                        Console.WriteLine($"📝 Claim Type: {claim.Type}, Value: {claim.Value}");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"❌ User NOT authenticated. Token: {context.Request.Headers["Authorization"]}");
-                }
-
-                await next();
-            });
-
-
+                       
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -51,23 +29,8 @@ namespace backend.Extensions
             {
                 AppDbContext dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
                 UserManager<User> userManager = scope.ServiceProvider.GetService<UserManager<User>>();
-
-                if (userManager.Users.Count() == 0)
-                {
-                dbContext.Database.Migrate();
-                    //Add admin user
-                    User user = new User
-                    {
-                        Id = "Superuser",
-                        FirstName = Environment.GetEnvironmentVariable("ADMIN_FIRSTNAME"),
-                        LastName = Environment.GetEnvironmentVariable("ADMIN_LASTNAME"),
-                        Email = Environment.GetEnvironmentVariable("ADMIN_EMAIL"),
-                        ProfileImg = Environment.GetEnvironmentVariable("ADMIN_PROFILEIMG") ?? "",
-                    };
-                    string password = Environment.GetEnvironmentVariable("ADMIN_PASSWORD");
-                    userManager.CreateAsync(user, password);
-                }
-
+                
+                dbContext.Database.Migrate();                              
             }
            
             return app;
