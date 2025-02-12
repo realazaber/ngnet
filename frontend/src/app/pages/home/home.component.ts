f18bdc71e4a633e3c16dde00f4f24773d4b32035
AZ <47402666+realazaber@users.noreply.github.com>
2025-02-12 22:19:28 +1100

    Frontend: Home Page

---
+++ frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [SharedModule, RouterModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomePage {}
