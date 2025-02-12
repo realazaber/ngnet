1165fa09a91842a104a7868017e9df9c03592331
AZ <47402666+realazaber@users.noreply.github.com>
2025-02-12 22:19:04 +1100

    Frontend: Login

---
+++ frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { LoginDTO } from '../../models/user/login.dto';
+import { AuthService } from '../../services/auth.service';
+import { TokenDTO } from '../../models/auth/token.dto';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { error } from 'console';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [SharedModule, FormsModule, RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginPage {
+  public formData: LoginDTO = {} as LoginDTO;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  async login() {
+    if (!this.formData.email || !this.formData.password) {
+      alert('Please fill in all fields');
+    }
+
+    this.authService.login(this.formData).subscribe((token: TokenDTO) => {
+      this.authService.saveToken(token);
+      this.router.navigateByUrl('/dashboard');
+    });
+  }
+}
