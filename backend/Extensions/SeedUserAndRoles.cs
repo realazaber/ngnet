242b28965624ff4ad2fccae26d18de325085273c
AZ <47402666+realazaber@users.noreply.github.com>
2025-02-18 07:46:36 +1100

    Feature: Seed user and roles.

---
+++ backend/Extensions/SeedUserAndRoles.cs
@@ -0,0 +1,46 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace backend.Extensions
+{
+    public static class SeedUserAndRoles
+    {
+        public static async Task CreateUserAndRolesAsync(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            RoleManager<IdentityRole> roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            UserManager<User> userManager = scope.ServiceProvider.GetService<UserManager<User>>();
+            
+
+            if (userManager.Users.Count() < 1)
+            {
+                string[] roleNames = { "Admin", "ManageUsers", "CanMakeFiles", "CanMakeFolders" };
+
+                foreach (var roleName in roleNames)
+                {
+                    if (!await roleManager.RoleExistsAsync(roleName))
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(roleName));
+                    }
+                }
+
+                //Add admin user
+                User user = new User
+                {
+                    Id = Guid.Empty.ToString(),
+                    FirstName = Environment.GetEnvironmentVariable("ADMIN_FIRSTNAME"),
+                    LastName = Environment.GetEnvironmentVariable("ADMIN_LASTNAME"),
+                    Email = Environment.GetEnvironmentVariable("ADMIN_EMAIL"),
+                    UserName = Environment.GetEnvironmentVariable("ADMIN_EMAIL"),
+                    ProfileImg = Environment.GetEnvironmentVariable("ADMIN_PROFILEIMG") ?? "",
+                };
+                string password = Environment.GetEnvironmentVariable("ADMIN_PASSWORD");
+                await userManager.CreateAsync(user, password);
+                await userManager.AddToRoleAsync(user, "Admin");
+                await userManager.AddToRoleAsync(user, "ManageUsers");
+                await userManager.AddToRoleAsync(user, "CanMakeFiles");
+                await userManager.AddToRoleAsync(user, "CanMakeFolders");
+            }
+        }
+    }
+}
