e143ed005cd729eff7d2e93442690d355f47d835
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-28 04:00:34 +1000

    Backend Config: Db Context

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using backend.Models;
+using backend.Models.Files;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public DbSet<User> Users { get; set; }
+       
+        public DbSet<DbEvent> Events { get; set; }        
+
+        public DbSet<IdentityRole> Roles { get; set; }
+
+        public DbSet<FileEntity> Files { get; set; }
+
+        public DbSet<FolderEntity> Folders { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+    }
+}
