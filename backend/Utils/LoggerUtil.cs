6d0e00d71e6c8f202633ef673af7fdee8e6f87dd
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-28 03:55:11 +1000

    Backend Util: Util logger.

---
+++ backend/Utils/LoggerUtil.cs
@@ -0,0 +1,35 @@
+﻿using Serilog;
+
+namespace backend.Utils
+{
+    public enum LogLevels
+    {
+        Information, Warning, Error
+    }
+    public static class PrintLogger
+    {
+        public static void PrintLog(string message, LogLevels level)
+        {
+            switch (level)
+            {
+                case LogLevels.Information:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Info: " + message);
+                    Log.Information(message);
+                    break;
+                case LogLevels.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Warning: " + message);
+                    Log.Warning(message);
+                    break;
+                case LogLevels.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error: " + message);
+                    Log.Error(message);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
