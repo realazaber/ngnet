891341dc85d04740f0fdcf31bdf267a1f20556ba
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-28 03:55:37 +1000

    UI: Auth guard

---
+++ frontend/src/app/guards/auth.guard.ts
@@ -0,0 +1,9 @@
+import { CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService: AuthService = inject(AuthService);
+
+  return authService.getAccessToken() !== '';
+};
