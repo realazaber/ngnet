4c5a55a8857c6ffa278935f92d9e7e8a6f5a57be
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-28 03:52:38 +1000

    UI: Reusable container component

---
+++ frontend/src/app/components/container/container.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngnet-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './container.component.html',
+  styles: ``,
+})
+export class ContainerComponent {}
