acc8b50fde71b2f7de6007463333edd22d31f387
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-28 03:54:54 +1000

    Backend: Generic Repositories

---
+++ backend/Repositories/Shared/Repository.cs
@@ -0,0 +1,58 @@
+﻿using backend.Data;
+using backend.DTOs.Shared;
+using backend.Models;
+
+namespace backend.Repositories.Shared
+{
+    public abstract class Repository<TEntity>
+        where TEntity : Entity
+        
+    {
+
+        private readonly AppDbContext _context;
+        protected Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public virtual TEntity GetById(Guid id)
+        {
+            return _context.Set<TEntity>().FirstOrDefault(x => x.Id == id);
+        }
+
+        public virtual void Create(TEntity entity) { 
+            _context.Set<TEntity>().Add(entity); 
+        }
+
+        public PagedResponse<List<TEntity>> GetMany(int pageNum, int pageSize)
+        {
+            var response = new PagedResponse<List<TEntity>>();
+            response.totalCount = _context.Set<TEntity>().Count();
+            response.pageNum = pageNum;
+            response.pageSize = pageSize;
+            response.data = _context.Set<TEntity>()
+                                    .Skip((pageNum - 1) * pageSize)
+                                    .Take(pageSize)
+                                    .ToList();
+            return response;
+        }
+
+
+        public IQueryable<TEntity> GetQueryable()
+        {
+            return _context.Set<TEntity>().AsQueryable();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ backend/Repositories/Shared/RepositoryAsync.cs
@@ -0,0 +1,60 @@
+﻿using backend.Data;
+using backend.DTOs.Shared;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repositories.Shared
+{
+    public abstract class RepositoryAsync<TEntity>
+        where TEntity : Entity
+        
+    {
+
+        protected readonly AppDbContext _context;
+        protected RepositoryAsync(AppDbContext context) 
+        {
+            _context = context;
+        }
+
+        public virtual async Task<TEntity> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<TEntity>().FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public virtual async Task CreateAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task<PagedResponse<List<TEntity>>> GetManyAsync(int pageNum, int pageSize)
+        {
+            PagedResponse<List<TEntity>> response = new PagedResponse<List<TEntity>>();
+            response.totalCount = await _context.Set<TEntity>().CountAsync();
+            response.pageNum = pageNum;
+            response.pageSize = pageSize;
+            response.data = await _context.Set<TEntity>()
+                                    .Skip((pageNum - 1) * pageSize)
+                                    .Take(pageSize)
+                                    .ToListAsync();
+            return response;
+        }
+
+        public virtual IQueryable<TEntity> GetQueryableAsync()
+        {
+            return _context.Set<TEntity>().AsQueryable();
+        }
+
+        public virtual async Task UpdateAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
