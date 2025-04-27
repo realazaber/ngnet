2fbae5adb319a787b580d3ae7f8e4f1cd762c4f7
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-28 03:49:51 +1000

    UI: Footer

---
+++ frontend/src/app/components/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'ngnet-footer',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './footer.component.html',  
+})
+export class FooterComponent {}
