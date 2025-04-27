352fb829fdbe6a81fc50f168465f895760e107db
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-28 03:55:56 +1000

    UI: DMS guard (checks if user is allowed to manage files).

---
+++ frontend/src/app/guards/dms.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { inject } from '@angular/core';
+import { UserDTO } from '../models/auth/user/user.dto';
+import { map, catchError, of } from 'rxjs';
+
+export const dmsGuard: CanActivateFn = (route, state) => {
+  const userService = inject(UserService);
+  const router = inject(Router);
+
+  return userService.getCurrentUser().pipe(
+    map((user: UserDTO) => {
+      console.log(user);
+      const authorized = user.roles.includes('Dms');
+      if (!authorized) {
+        router.navigate(['login']);
+      }
+      console.log(authorized);
+      return authorized;
+    }),
+    catchError(() => {
+      router.navigate(['login']);
+      console.log('fail');
+      return of(false);
+    })
+  );
+};
