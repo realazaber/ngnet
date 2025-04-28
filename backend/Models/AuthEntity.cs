be01634a5e0f1885b9ebbeba655bf03399165233
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-29 01:32:58 +1000

    Models for entities

---
+++ backend/Models/AuthEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace backend.Models
+{
+    public abstract class AuthEntity : Entity
+    {
+        public Guid CreatorId { get; set; }
+    }
+}
---
+++ backend/Models/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public abstract class Entity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+    }
+}
