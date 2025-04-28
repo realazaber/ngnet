314314e64dc732bbf13abd49898bba044627e48f
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-28 23:55:03 +1000

    Refactor: Change UI route names.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,9 +6,13 @@ import { ErrorPage } from './pages/error/error.component';
 import { DashboardPage } from './pages/auth/dashboard/dashboard.component';
 import { authGuard } from './guards/auth.guard';
 import { ProfilePage } from './pages/auth/profile/profile.component';
-import { ManageusersPage } from './pages/auth/manageusers/manageusers.component';
+import { ViewUsersPage } from './pages/auth/user/view/manageusers.component';
 import { ManageUsersGuard } from './guards/manageusers.guard';
-import { CreateUserPage } from './pages/auth/createuser/createuser.component';
+import { CreateUserPage } from './pages/auth/user/create/createuser.component';
+import { DmsPage } from './pages/auth/dms/dms.component';
+import { dmsGuard } from './guards/dms.guard';
+import { EditUserPage } from './pages/auth/user/edit/edit.component';
+import { ManageRolesPage } from './pages/auth/manageroles/manageroles.component';
 
 export const routes: Routes = [
   {
@@ -28,28 +32,46 @@ export const routes: Routes = [
   },
 
   {
-    path: 'dashboard',
+    path: 'auth/dashboard',
     component: DashboardPage,
     pathMatch: 'full',
     canActivate: [authGuard],
   },
   {
-    path: 'createuser',
+    path: 'auth/dms',
+    component: DmsPage,
+    pathMatch: 'full',
+    canActivate: [authGuard, dmsGuard],
+  },
+  {
+    path: 'auth/createuser',
     component: CreateUserPage,
     pathMatch: 'full',
     canActivate: [authGuard, ManageUsersGuard],
   },
   {
-    path: 'profile',
-    component: ProfilePage,
+    path: 'auth/edituser/:id',
+    component: EditUserPage,
+    pathMatch: 'full',
+    canActivate: [authGuard, ManageUsersGuard],
+  },
+  {
+    path: 'auth/viewusers',
+    component: ViewUsersPage,
+    pathMatch: 'full',
+    canActivate: [authGuard, ManageUsersGuard],
+  },
+  {
+    path: 'auth/manageroles',
+    component: ManageRolesPage,
     pathMatch: 'full',
     canActivate: [authGuard],
   },
   {
-    path: 'manageusers',
-    component: ManageusersPage,
+    path: 'auth/profile',
+    component: ProfilePage,
     pathMatch: 'full',
-    canActivate: [authGuard, ManageUsersGuard],
+    canActivate: [authGuard],
   },
   {
     path: '**',
