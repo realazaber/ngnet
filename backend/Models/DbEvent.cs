ef084be597ead68fccdb9ff4cee8eef01ea101e1
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-30 06:56:16 +1000

    Backend - Model: Db Event, model for saving events that relate to database operations.

---
+++ backend/Models/DbEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class DbEvent : Entity
+    {
+            
+        public string? Description { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public User User { get; set; }
+    }
+}
