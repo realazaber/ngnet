7626a24fa311f1dc422e86ae96849e89aae9a6f8
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-30 04:14:05 +1000

    Backend feature: Base Folder controller

---
+++ backend/Controllers/FolderController.cs
@@ -0,0 +1,47 @@
+﻿using backend.DTOs.File;
+using backend.Services;
+using Backend.DTOs.File;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Authorize(Roles = "Dms")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FolderController : ControllerBase
+    {
+        private readonly FolderService _folderService;
+
+        public FolderController(FolderService folderService)
+        {
+            _folderService = folderService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFolderContents([FromQuery] Guid? folderId)
+        {
+            List<GetFolderContentsDTO> contents = await _folderService.GetFolderContents(folderId);
+
+            return Ok(contents);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFolder([FromBody] CreateFolderDTO createFolderDTO)
+        {
+            return await _folderService.CreateFolder(createFolderDTO, User);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditFolder([FromQuery] Guid folderId, [FromBody] EditFolderDTO dto)
+        {
+            return await _folderService.EditFolder(folderId, dto.NewName, dto.UserIds, User);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteFolder([FromQuery] Guid folderId)
+        {
+            return await _folderService.DeleteFolder(folderId, User);
+        }
+    }
+}
