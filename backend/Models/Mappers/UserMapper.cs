340ffca433c1b33d394c61350bd2a4e0dd419402
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-29 22:28:08 +1000

    Backend util: User mapper

---
+++ backend/Models/Mappers/UserMapper.cs
@@ -0,0 +1,15 @@
+﻿using backend.DTOs.User;
+using backend.Models;
+
+namespace Backend.Models.Mappers
+{
+    public static class UserMapper
+    {
+        public static RetriveUserDTO ConvertToRetrieveDTO(User user, IList<string> roles)
+        {
+            return new RetriveUserDTO(new Guid(user.Id), user.FirstName, user.LastName,
+                                                              user.ProfileImg, user.Email, user.UserName,                                                              user.PhoneNumber, user.CreatorId, roles);
+        }
+
+    }
+}
