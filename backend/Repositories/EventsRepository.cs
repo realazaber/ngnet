9efd475042c0dd10bcdd827276268429ec571ec1
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-30 06:56:34 +1000

    Backend: Base repository for CRUD operations on Events

---
+++ backend/Repositories/EventsRepository.cs
@@ -0,0 +1,17 @@
+﻿using backend.Data;
+using backend.Models;
+using backend.Repositories.Shared;
+
+namespace backend.Repositories
+{
+    public class EventsRepository: RepositoryAsync<DbEvent>
+    {
+        
+        public EventsRepository(AppDbContext context) : base(context)
+        {
+         
+        }
+
+        
+    }
+}
