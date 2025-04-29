97d2137d7112659344b9bfc97e6388f3bfca8da4
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-30 04:12:21 +1000

    Frontend Feature: Base upload file page

---
+++ frontend/src/app/components/filesystem/file/upload-file/upload-file.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-file',
+  standalone: true,
+  imports: [],
+  templateUrl: './upload-file.component.html',
+  styles: ``
+})
+export class UploadFileComponent {
+
+}
