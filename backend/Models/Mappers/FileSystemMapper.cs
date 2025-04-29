7a8456ff79fe5ae4d6aeff1ef1ad6b727ae98350
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-29 22:28:14 +1000

    Backend util: File system mapper

---
+++ backend/Models/Mappers/FileSystemMapper.cs
@@ -0,0 +1,49 @@
+﻿using backend.Models.Files;
+using Backend.DTOs.File;
+
+namespace Backend.Models.Mappers
+{
+    public static class FileSystemMapper
+    {
+        public static GetFileSystemEntityDTO FolderToFileSystemDTO(FolderEntity folder) { 
+            return new GetFileSystemEntityDTO(folder.Id, 
+                                              folder.Name,
+                                              folder.Description,
+                                              folder.CreatorId,
+                                              folder.CreatedDate,
+                                              folder.UpdatedDate);
+        }
+
+        public static GetFileSystemEntityDTO FileToFileSystemDTO(FileEntity file) {
+            return new GetFileSystemEntityDTO(file.Id, 
+                                              file.Name, 
+                                              file.Description, 
+                                              file.CreatorId, 
+                                              file.CreatedDate, 
+                                              file.UpdatedDate);
+        }
+
+        public static GetFolderContentsDTO FileToFolderContentsDTO(FileEntity file) {
+            return new GetFolderContentsDTO(file.Id,
+                                            file.Name,
+                                            file.Description,
+                                            file.Path,
+                                            file.CreatedDate,
+                                            file.UpdatedDate,
+                                            false,
+                                            file.FolderId ?? null);
+        }
+
+        public static GetFolderContentsDTO FolderToFolderContentsDTO(FolderEntity folder) {
+            return new GetFolderContentsDTO(folder.Id,
+                                            folder.Name,        
+                                            folder.Description,
+                                            folder.Path,
+                                            folder.CreatedDate,
+                                            folder.UpdatedDate,
+                                            true,
+                                            folder.FolderId ?? null);
+        }
+            
+    }
+}
