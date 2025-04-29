b585f75525d5ba6e5284ce5103ae07b909cd8c65
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-29 22:28:24 +1000

    Backend: Base response objects

---
+++ backend/DTOs/Shared/PagedResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace backend.DTOs.Shared
+{
+    public class PagedResponse<T>
+    {
+        public T data {  get; set; }
+
+        public int pageNum { get; set; }
+
+        public int pageSize { get; set; }
+
+        public int totalCount { get; set; }
+    }
+    
+}
---
+++ backend/DTOs/Shared/Response.cs
@@ -0,0 +1,6 @@
+﻿namespace Backend.DTOs.Shared
+{
+    public class Response
+    {
+    }
+}
